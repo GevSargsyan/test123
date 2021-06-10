c0f4d729847b9de0e5b31221947fceb67d7150b1
Gev <Gev@DESKTOP-FN6RES1>
2021-06-10 20:39:41 +0400

    test3

---
+++ testtest1/testest3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testest3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testest3");
+        }
+    }
+}
